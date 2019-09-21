1e137ba1c2389ab6c595e96c66acae4fc67835cb
Al145 <47367383+Al145@users.noreply.github.com>
2019-09-21 19:46:56 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const openModalButton = document.querySelectorAll('[data-modal-target]');
+const closeModalButton = document.querySelectorAll('[data-close-button]');
+const uninstallModalButton = document.querySelectorAll('[data-uninstall-button]');
+const overlay = document.querySelector('#overlay');
+
+openModalButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector(button.dataset.modalTarget);
+        openModal(modal);
+    })
+});
+
+closeModalButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+    })
+});
+
+uninstallModalButton.forEach(button => {
+    button.addEventListener('click', () => {
+        const modal = button.closest('.modal');
+        closeModal(modal);
+        alert('Done');
+    })
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active');
+    overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active');
+    overlay.classList.remove('active');
+}
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+        closeModal(modal);
+    })
+})
